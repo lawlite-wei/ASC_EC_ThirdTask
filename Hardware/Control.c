496b6288c7eeb0f74210650e6a24fe461b412cba
lawlite-wei <lwqxbq@qq.com>
2025-11-22 23:56:16 +0800

    配置循迹模块

---
+++ Hardware/Control.c
@@ -0,0 +1,8 @@
+#include "stm32f10x.h"                  // Device header
+#include "Motor.h"
+#include "Tarce.h"
+
+void Control_Task(void)
+{
+	
+}
---
+++ Hardware/Tarce.h
@@ -0,0 +1,11 @@
+#ifndef __TARCE_H
+#define __TARCE_H
+
+#define X1 GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_11)
+#define X2 GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_10)
+#define X3 GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_0)
+#define X4 GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_1)
+
+void Tarce_Init(void);
+
+#endif
